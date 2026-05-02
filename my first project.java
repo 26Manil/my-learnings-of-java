4ee33f01c310ea70b455ac89cb4fb6d0d2b2c77b
26Manil <manilmistry66@gmail.com>
2026-05-02 13:11:26 +0530

    Create a user input program with motivational output
    This Java program collects user input for name, age, JEE rank, and strongest subject, and provides motivational feedback based on the user's choice to give up or not.

---
+++ my first project.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args)
+    {
+        // Getting my hands on Java Scanner for the first time!
+        // This little program asks who you are, where you stand in JEE,
+        // and reminds you that giving up was never really an option.
+        // leaning how to accept user input through different variables
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter your name ");
+        String name = scanner.nextLine();
+        System.out.println("please enter your age ");
+        int age = scanner.nextInt();
+        System.out.println(" enter your jee rank");
+        double rank = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("wich is your strongest subject ");
+        String Subject = scanner.nextLine();
+
+        System.out.println("are you choosing to give up(true/false)  ");
+        boolean alive = scanner.nextBoolean();
+        System.out.println(" You are " + name + " and you are " + age +" years old and your rank is  " + rank
+      + " and you have " + Subject + " as a strong subject \n you may feel exhausted from this all but,\n running away from it will never fix it, so if it is "  );
+
+        if(alive){
+            System.out.println(" true then ,please don't give up");
+        }
+        else{
+        System.out.println(" false , then keep moving forwad, you are on right path" );
+
+    }
+
+        scanner.close();
+    }
+}
