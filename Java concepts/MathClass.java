cbf7e66bc7ddb91a6176cc2b92a5c613dafae3d6
26Manil <manilmistry66@gmail.com>
2026-08-15 17:04:09 +0530

    correction

--- Java concepts/MathClass.java
+++ Java concepts/MathClass.java
@@ -4,13 +4,13 @@ public class MathClass {
         Pie = Math.PI
         Euclades number = Math.E
         For using Powers = Math.pow(2,4)
-        Math.abs(-5);      (gives the nummber as positive on printing it.)
-        Math.sqrt(9);      (this will output 3 ) (gives square root on printing)
-        Math.round(3.14) , (gives round off value  on printing it,this will retun 3)
-        Math.ceil(3.14);   (This will round up on printing)
-        Math.floor(3.99);  (This will round down on printing)
-        Math.max(10,20);   (this will print maximum value between both the numbers. in this case it will be 20)
-        Math.min(10,20);   (this will print minimum value between both the numbers. in this case it will be 10)
+        Math.abs(-5)       (gives the nummber as positive on printing it.)
+        Math.sqrt(9)       (this will output 3 ) (gives square root on printing)
+        Math.round(3.14)   (gives round off value  on printing it,this will retun 3)
+        Math.ceil(3.14)    (This will round up on printing)
+        Math.floor(3.99)   (This will round down on printing)
+        Math.max(10,20)    (this will print maximum value between both the numbers. in this case it will be 20)
+        Math.min(10,20)    (this will print minimum value between both the numbers. in this case it will be 10)
         */
        System.out.println(Math.PI);
        System.out.println(Math.pow(2,4));
