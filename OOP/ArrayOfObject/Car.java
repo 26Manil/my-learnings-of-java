84d068f9ec2d3db53aa849f05899dcfb4f8b0232
26Manil <manilmistry66@gmail.com>
2026-08-25 21:05:46 +0530

    array object

---
+++ OOP/ArrayOfObject/Car.java
@@ -0,0 +1,15 @@
+package ArrayOfObject;
+
+public class Car {
+
+    String model;
+    String colour;
+    Car(String model,String colour){
+        this.model = model;
+        this.colour = colour;
+    }
+    void drive(){
+        System.out.println("You  are driving the " + this.colour +" " + this.model);
+    }
+    
+}
---
+++ OOP/ArrayOfObject/main.java
@@ -0,0 +1,19 @@
+package ArrayOfObject;
+
+public class main {
+    public static void main(String[] args) {
+
+        
+        Car[] cars = { new Car ("BMW","Black"),
+                       new Car ("Mustang","Blue"),
+                        new Car ("Porshe","Red") };
+         for(Car car : cars){
+            car.drive();
+         }
+         for(Car car : cars){
+
+            car.colour = "black";
+         }
+    }
+    
+}
