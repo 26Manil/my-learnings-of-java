614e43d79e497c6f68f4cc2bd8fa41ecf4d33c7c
26Manil <manilmistry66@gmail.com>
2026-09-12 00:35:19 +0530

    to string

---
+++ OOP/toString/Car.java
@@ -0,0 +1,19 @@
+package toString;
+
+public class Car {
+    String make;
+    String model;
+    int year;
+    String colour;
+    
+    Car ( String make, String model,int year,String colour){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.colour = colour;
+    }
+    @Override
+    public String toString(){
+        return this.colour + " " + this.year + " " + this.make + " " + this.model;
+    }
+}
---
+++ OOP/toString/main.java
@@ -0,0 +1,19 @@
+package toString;
+
+public class main {
+
+    /*
+    
+    toString() = Method inharits from class object class use to 
+    representation of an object by default , it return a hash code a unique 
+    identifier it can be overrided to provide meaningfull detail.
+
+
+    */
+
+    public static void main(String[] args) {
+          Car car1 = new Car("BMW","M4",2026,"Blue");
+          System.out.println(car1);
+    }
+    
+}
